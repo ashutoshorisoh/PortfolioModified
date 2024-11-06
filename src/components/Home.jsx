76b50514660a72d97a2455e040309362770ad63b
Ashutosh <ashutoshorisoh@gmail.com>
2024-11-06 17:43:56 +0530

    img dir fixed

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ function Home() {
   return (
     <div className='h-auto p-10 w-full flex flex-col sm:flex-row gap-5 justify-center items-center bg-blue-300'>
       <div className='flex justify-center items-center shadow-2xl'>
-        <img src="src/assets/newt.png" alt="Profile" className='h-40 w-44 object-contain' />
+        <img src="/assetts/newt.png" alt="Profile" className='h-40 w-44 object-contain' />
       </div>
       <div className='w-full sm:w-[80%] p-5 flex justify-center items-center text-center flex-col gap-5'>
         <h1 className="text-4xl font-bold">Hi, I am Ashutosh</h1>

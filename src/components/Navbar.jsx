ace891b9b236c70dc043609b3ef0f701590f9dc9
Ashutosh <ashutoshorisoh@gmail.com>
2024-11-08 06:10:43 +0530

    font changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,28 +2,28 @@ import React from 'react';
 
 function Navbar({ scrollToHome, scrollToProjects, scrollToContact, scrollToMain }) {
   return (
-    <div className="bg-orange-200 font-mono lg:h-16 w-full flex flex-row lg:justify-end justify-center items-center text-black lg:text-xl text-sm font-semibold lg:pr-10 pr-0 lg:gap-5 gap-2 p-5 sticky top-0 z-10">
+    <div className="bg-orange-200 font-mono lg:h-16 w-full flex flex-row lg:justify-end justify-center items-center text-black lg:text-xl text-sm  lg:pr-10 pr-0 lg:gap-5 gap-4 p-5 sticky top-0 z-10">
       <button 
-        className="text-black hover:text-gray-400"
+        className="text-black hover:text-gray-400 font-bold"
         onClick={scrollToMain} // Scroll to Main Page
       >
         Home
       </button>
       <button 
-        className="text-black hover:text-gray-400"
+        className="text-black hover:text-gray-400 font-bold"
         onClick={scrollToHome} // Scroll to About Me
       >
         About Me
       </button>
       <button 
-        className="text-black hover:text-gray-400"
+        className="text-black hover:text-gray-400 font-bold"
         onClick={scrollToProjects} // Scroll to Projects
       >
         Projects
       </button>
       
       <button 
-        className="text-black hover:text-gray-400"
+        className="text-black hover:text-gray-400 font-bold"
         onClick={scrollToContact} // Scroll to Contact Me
       >
         Contact Me
